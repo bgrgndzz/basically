def3b9a9a1332d3cd1b67c912bcdded76d3f2284
Buğra Gündüz <bgrgndzz@gmail.com>
2018-10-06 01:45:56 +0300

    render Home

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,35 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
+import Home from './components/Home/Home';
+
 export default class App extends Component {
+  state = {
+    page: 'Home'
+  }
+  
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hello World</Text>
-      </View>
-    );
+    switch(this.state.page) {
+      case 'Home':
+        return <Home />;
+      default:
+        // temporary error view
+        return (
+          <View style={styles.errorContainer}>
+            <Text style={styles.errorText}>An unknown error occurred.</Text>
+          </View>
+        );
+    }
   }
 }
 
 const styles = StyleSheet.create({
-  container: {}
+  errorContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  errorText: {
+    fontSize: 24
+  }
 });
